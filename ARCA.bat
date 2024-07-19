e83a7d69a348749a140950563988a93e557c4852
Brian Freitas <brian_pereira98@hotmail.com>
2024-07-19 17:30:45 -0300

    Update ARCA.bat

--- ARCA.bat
+++ ARCA.bat
@@ -1,2 +1,1 @@
-start /min
-"C:\Users\DEV\AppData\Local\Programs\Python\Python310\python.exe" "C:/Users/DEV/OneDrive/ARCA/ARCA_main.py"
+start /min "C:\Users\DEV\AppData\Local\Programs\Python\Python310\python.exe" "C:/Users/DEV/OneDrive/ARCA/ARCA_main.py"
