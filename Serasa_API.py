07178f4d93f996ab7f1242ea938b2d91c25568ee
Brian Freitas <brian_pereira98@hotmail.com>
2025-04-22 10:16:17 -0300

    update

--- Serasa_API.py
+++ Serasa_API.py
@@ -70,6 +70,6 @@ def serasa_result(CPF):
 
 
 if __name__ == "__main__":
-  dados = serasa_result("46356000848")
+  dados = serasa_result("43739069805")
   print(dados)
   # create_json("Washington Rocha",serasa_result("04419575360"))
